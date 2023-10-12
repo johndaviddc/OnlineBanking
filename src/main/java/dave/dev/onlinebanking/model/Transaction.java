cfe2aa5d7803b0a9aa66bbf1e8fe43824791332f
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-12 16:12:25 +0800

    Online Banking

---
+++ src/main/java/dave/dev/onlinebanking/model/Transaction.java
@@ -0,0 +1,59 @@
+package dave.dev.onlinebanking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sender;
+    private String receiver;
+    private double amount;
+    private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/test/java/dave/dev/onlinebanking/OnlinebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.onlinebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
