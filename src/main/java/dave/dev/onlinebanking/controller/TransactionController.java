b8ccab3dcdc20bde06a42de4ab9fe66ed042d23b
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-13 15:28:42 +0800

    added get mapping, getAllTransactions

--- src/main/java/dave/dev/onlinebanking/controller/TransactionController.java
+++ src/main/java/dave/dev/onlinebanking/controller/TransactionController.java
@@ -5,10 +5,9 @@ import dave.dev.onlinebanking.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -25,5 +24,10 @@ public class TransactionController {
     public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
         Transaction createdTransaction = transactionService.createTransaction(transaction);
         return new ResponseEntity<>(createdTransaction, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionService.getAllTransactions();
     }
 }
