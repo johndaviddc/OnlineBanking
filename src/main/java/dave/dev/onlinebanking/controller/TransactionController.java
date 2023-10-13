5aacd1f33bd28ed5f9b09146a157687b008ce4bb
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-13 15:27:18 +0800

    added post mapping

--- src/main/java/dave/dev/onlinebanking/controller/TransactionController.java
+++ src/main/java/dave/dev/onlinebanking/controller/TransactionController.java
@@ -1,7 +1,12 @@
 package dave.dev.onlinebanking.controller;
 
+import dave.dev.onlinebanking.model.Transaction;
 import dave.dev.onlinebanking.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,11 @@ public class TransactionController {
     @Autowired
     public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
+        Transaction createdTransaction = transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(createdTransaction, HttpStatus.CREATED);
     }
 }
