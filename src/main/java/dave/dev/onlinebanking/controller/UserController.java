ec5b4fe68840870b11211e9ed7196e246b850b30
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-13 15:10:48 +0800

    added controller package

---
+++ src/main/java/dave/dev/onlinebanking/controller/UserController.java
@@ -0,0 +1,29 @@
+package dave.dev.onlinebanking.controller;
+
+import dave.dev.onlinebanking.model.User;
+import dave.dev.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
