0249e2326936412f971c97d357491cbabd909aa2
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-13 14:57:01 +0800

    added service

---
+++ src/main/java/dave/dev/onlinebanking/service/TransactionService.java
@@ -0,0 +1,32 @@
+package dave.dev.onlinebanking.service;
+
+import dave.dev.onlinebanking.model.Transaction;
+import dave.dev.onlinebanking.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Optional<Transaction> getTransactionById(Long id) {
+        return transactionRepository.findById(id);
+    }
+}
