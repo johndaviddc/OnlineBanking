760be10577fca90893afd678f04c694f23f3c21c
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-13 15:13:48 +0800

    added get mapping, get all users

--- src/main/java/dave/dev/onlinebanking/controller/UserController.java
+++ src/main/java/dave/dev/onlinebanking/controller/UserController.java
@@ -5,10 +5,9 @@ import dave.dev.onlinebanking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,5 +24,10 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
