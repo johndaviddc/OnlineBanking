80439dfc59d6bcf09cab34cf32ef2129d38995f0
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-13 15:25:22 +0800

    autowired transaction service

--- src/main/java/dave/dev/onlinebanking/controller/TransactionController.java
+++ src/main/java/dave/dev/onlinebanking/controller/TransactionController.java
@@ -1,4 +1,18 @@
 package dave.dev.onlinebanking.controller;
 
+import dave.dev.onlinebanking.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
 }
